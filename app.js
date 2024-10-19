5ad96addf76a44480438358a1b4611937cd2c74f
Scott Pickering <scottpickering31@gmail.com>
2024-10-19 13:17:22 +0100

    added ecosystem.config.cjs

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import express from "express";
 import router from "./routes/tokenRoutes.js";
 import db from "./integration/database/db.js";
-import "./integration/telegramApi/telegramButtons.js";
 
 // Create express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
