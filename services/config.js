2ced4f4c067ae495d5ae1d9db1f11a27c34fb2f5
Scott Pickering <scottpickering31@gmail.com>
2025-01-31 10:25:57 +0000

    config update

--- services/config.js
+++ services/config.js
@@ -3,10 +3,9 @@ const bs58 = require("bs58").default;
 const { Wallet } = require("@project-serum/anchor");
 
 const HELIUS_KEY = process.env.HELIUS_API_KEY;
+const RPC_URL = process.env.RPC_URL;
 
-const connection = new Connection(
-  `https://mainnet.helius-rpc.com/?api-key=${HELIUS_KEY}`
-);
+const connection = new Connection(`${RPC_URL}?api-key=${HELIUS_KEY}`);
 
 const wallet = new Wallet(
   Keypair.fromSecretKey(bs58.decode(process.env.WALLET_PRIVATE_KEY || ""))
